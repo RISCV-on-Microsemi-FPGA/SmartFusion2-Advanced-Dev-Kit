ea09e59ddd9229edd11c1d991070acaab9d6b0c8
Sebastian <Seb.Slowikowski@microchip.com>
2020-02-28 15:06:58 +0000

    Libero v12.3 design revamp (12.3-v2.0)
    This update includes a redesign of the TCL scripts.
    * The MSS subsystem has been removed from the design. All components are found on BaseDesign canvas.
    * The BaseDesign has been simplified to contain only vital components.
    * The DDR has been replaced by LSRAM
    * Names of files, directories and components on the canvas.
    * Improved strcture of the TCL scripts (different component instantiation technique)
    * Updated Libero core components.

---
+++ Libero_Projects/import/components/M2S150TS-1FC1152/AHB/import_component_and_constraints_sf2_adv_dev_kit_ts_ahb.tcl
@@ -0,0 +1,41 @@
+set project_folder_name MiV_AHB_SF2_Adv_Dev_Kit_TS
+set project_dir2 "./$project_folder_name"
+set Libero_project_name MiV_AHB_BaseDesign
+
+
+puts "-------------------------------------------------------------------------"
+puts "-----------------------IMPORTING COMPONENTS------------------------------"
+puts "-------------------------------------------------------------------------" 
+
+
+source ./import/components/M2S150TS-1FC1152/AHB/top_level_sf2_adv_dev_kit_ts_ahb.tcl
+
+puts "-------------------------------------------------------------------------"
+puts "--------------------APPLYING DESING CONSTRAINTS--------------------------"
+puts "-------------------------------------------------------------------------"
+
+import_files -io_pdc ./import/constraints/io/io_constraints.pdc
+import_files -io_pdc ./import/constraints/io/user.pdc
+import_files -sdc    ./import/constraints/io_jtag_constraints.sdc
+
+build_design_hierarchy
+set_root BaseDesign
+
+run_tool -name {CONSTRAINT_MANAGEMENT}
+# #Associate SDC constraint file to Place and Route tool
+organize_tool_files -tool {PLACEROUTE} \
+    -file $project_dir2/constraint/io_jtag_constraints.sdc \
+    -file $project_dir2/constraint/io/io_constraints.pdc \
+	-file $project_dir2/constraint/io/user.pdc \
+    -module {BaseDesign::work} -input_type {constraint}  
+    
+organize_tool_files -tool {SYNTHESIZE} \
+    -file $project_dir2/constraint/io_jtag_constraints.sdc \
+    -module {BaseDesign::work} -input_type {constraint}    
+    
+organize_tool_files -tool {VERIFYTIMING} \
+    -file $project_dir2/constraint/io_jtag_constraints.sdc \
+    -module {BaseDesign::work} -input_type {constraint}    
+
+derive_constraints_sdc 
+set_root BaseDesign
